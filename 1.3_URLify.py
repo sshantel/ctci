5c0f322efbc0471dea49d3d0cfa0b7b9487c8f00
Chantel Yip <chantelyip422@gmail.com>
2020-08-11 14:46:05 -0700

    problem not complete, trying to fix the space portion of it

--- 1.3_URLify.py
+++ 1.3_URLify.py
@@ -5,16 +5,26 @@ and that you are given the "true" length of the string.
 (Note: if implementing in Java, please use a character array so that you can perform 
 this operation in place.)
 
->>> to_urlify('Mr John Smith    ')
+>>> to_urlify('Mr John Smith    ', 13)
 'Mr%20John%20Smith'
 
->>> to_urlify('cracking the coding interview')
+>>> to_urlify('cracking the coding interview', 29)
 'cracking%20the%20coding%20interview'
 """
 
 
-def to_urlify(string):
-    return string.strip().replace(" ", "%20")
+# def to_urlify(string):
+#     return string.strip().replace(" ", "%20")
+
+# n = size of string - ignoring ending white spaces
+
+
+def to_urlify(string, n):
+    string_list = list(string)
+    for index, element in enumerate(string_list):
+        if element == " ":
+            string_list[index] = "%20"
+    return "".join(string_list)
 
 
 if __name__ == "__main__":
