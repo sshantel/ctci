9270e63fd27e85ee18bf513673f7e66731ffdebe
Chantel Yip <chantelyip422@gmail.com>
2020-08-11 17:49:20 -0700

    completed function

--- 1.7_rotate_matrix.py
+++ 1.7_rotate_matrix.py
@@ -4,23 +4,18 @@ is 4 bytes, write a method to rotate the image by 90 degrees.
 Can you do this in place?
 """
 
-n = 4
+
+def rotate_matrix(the_array):
+    res = []
+    for i in range(len(the_array)):
+        print(i)
+        transposed = []
+        for col in the_array:
+            print(col[i])
+            transposed.append(col[i])
+        transposed.reverse()
+        res.append(transposed)
+    return res
 
 
-def rotate_matrix(matrix):
-    for x in range(0, (n / 2)):
-        for y in range(x, n - x - 1):
-            temp = matrix[x][y]
-            matrix[x][y] = matrix[y][n - 1 - x]
-            matrix[y][n - 1 - x] = matrix[n - 1 - x][n - 1 - y]
-            matrix[n - 1 - x][n - 1 - y] = matrix[n - 1 - y][x]
-            matrix[n - 1 - y][x] = temp
-
-    def display_matrix(matrix):
-        for i in range(0, N):
-            for j in range(0, N):
-                print(mat[i][j], end=" ")
-
-
-print(rotate_matrix(4))
-print(display_matrix(4))
+print(rotate_matrix([[1, 2, 3, 4], [4, 5, 6, 7], [7, 8, 9, 10]]))
